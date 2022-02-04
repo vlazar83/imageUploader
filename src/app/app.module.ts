9dcf5753e371a88bed1081d41a8abd386c4c1f5e
Viktor Lazar <viktor.lazar@sap.com>
2022-02-04 09:44:55 +0100

    file upload - newer version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { DndDirective } from './directives/dnd.directive';
 import { AppComponent } from './app.component';
+import { FileUploadComponent } from './file-upload/file-upload.component';
+import { ProgressComponent } from './progress/progress.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileUploadComponent,
+    ProgressComponent,
+    DndDirective
   ],
   imports: [
     BrowserModule,
